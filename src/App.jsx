02df440464a449576b537bed118a6854e763ee64
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-06-20 20:23:48 -0300

    linted code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import './App.css'
 import CountryCard from './components/CountryCard'
-import NavBar from './components/NavBar';
-import { useState } from 'react'
-import { useEffect } from 'react';
-import FilterBar from './components/FilterBar';
+import NavBar from './components/NavBar'
+import { useState, useEffect } from 'react'
+import FilterBar from './components/FilterBar'
 
-function App() {
+function App () {
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
 
@@ -17,45 +15,46 @@ function App() {
         setCountries(data)
         setFilteredCountries(data)
       })
-    }, [])  
+  }, [])
 
-  function filterCountries(searchString) {
+  function filterCountries (searchString) {
     const filteredCountries = countries.filter(country => (country.name.common.toLowerCase()).includes(searchString.toLowerCase()))
     setFilteredCountries(filteredCountries)
   }
 
-  function filterByRegion(searchString) {
+  function filterByRegion (searchString) {
     const filteredCountries = countries.filter(country => (country.region.toLowerCase()).includes(searchString.toLowerCase()))
     setFilteredCountries(filteredCountries)
   }
 
-  let showCountries = filteredCountries.map((country, index) => {
+  const showCountries = filteredCountries.map((country, index) => {
     const { name, currencies, capital, region, subregion, languages, demonyms, area, population, flags } = country
     return (
-    <CountryCard
-    key={index}
-    name={name.common}
-    capital={capital}
-    region={region}
-    subregion={subregion}
-    currencies={currencies}
-    languages={languages}
-    area={area}
-    demonyms={demonyms}
-    population={population}
-    flag={flags.png} />
+      <CountryCard
+        key={index}
+        name={name.common}
+        capital={capital}
+        region={region}
+        subregion={subregion}
+        currencies={currencies}
+        languages={languages}
+        area={area}
+        demonyms={demonyms}
+        population={population}
+        flag={flags.png}
+      />
     )
   })
 
   return (
-    <div className="App">
-      <NavBar theme="dark" />
+    <div className='App'>
+      <NavBar theme='dark' />
       <FilterBar handleCountry={filterCountries} handleRegion={filterByRegion} />
-      <div id="countries">
+      <div id='countries'>
         {showCountries}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
